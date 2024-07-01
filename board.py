3940eb9d24ae140a26fd6885ef5a86ef08834c09
taylorandstubbs <taylor@taylorstubbs.com>
2024-07-01 19:57:23 -0400

    Refactor board.py to improve readability and maintainability

--- board.py
+++ board.py
@@ -13,135 +13,127 @@ class Board:
 
     @classmethod
     def clone(cls, chessboard):
-        chesspieces = [[0 for x in range(Board.WIDTH)] for y in range(Board.HEIGHT)]
+        chesspieces = [[0 for _ in range(Board.WIDTH)] for _ in range(Board.HEIGHT)]
         for x in range(Board.WIDTH):
             for y in range(Board.HEIGHT):
                 piece = chessboard.chesspieces[x][y]
-                if (piece != 0):
+                if piece != 0:
                     chesspieces[x][y] = piece.clone()
         return cls(chesspieces, chessboard.white_king_moved, chessboard.black_king_moved)
 
     @classmethod
     def new(cls):
-        chess_pieces = [[0 for x in range(Board.WIDTH)] for y in range(Board.HEIGHT)]
-        # Create pawns.
+        chess_pieces = [[0 for _ in range(Board.WIDTH)] for _ in range(Board.HEIGHT)]
+        cls._create_pawns(chess_pieces)
+        cls._create_rooks(chess_pieces)
+        cls._create_knights(chess_pieces)
+        cls._create_bishops(chess_pieces)
+        cls._create_royalty(chess_pieces)
+        return cls(chess_pieces, False, False)
+
+    @staticmethod
+    def _create_pawns(chess_pieces):
         for x in range(Board.WIDTH):
             chess_pieces[x][Board.HEIGHT-2] = pieces.Pawn(x, Board.HEIGHT-2, pieces.Piece.WHITE)
             chess_pieces[x][1] = pieces.Pawn(x, 1, pieces.Piece.BLACK)
 
-        # Create rooks.
-        chess_pieces[0][Board.HEIGHT-1] = pieces.Rook(0, Board.HEIGHT-1, pieces.Piece.WHITE)
-        chess_pieces[Board.WIDTH-1][Board.HEIGHT-1] = pieces.Rook(Board.WIDTH-1, Board.HEIGHT-1, pieces.Piece.WHITE)
-        chess_pieces[0][0] = pieces.Rook(0, 0, pieces.Piece.BLACK)
-        chess_pieces[Board.WIDTH-1][0] = pieces.Rook(Board.WIDTH-1, 0, pieces.Piece.BLACK)
+    @staticmethod
+    def _create_rooks(chess_pieces):
+        positions = [(0, Board.HEIGHT-1), (Board.WIDTH-1, Board.HEIGHT-1), (0, 0), (Board.WIDTH-1, 0)]
+        colors = [pieces.Piece.WHITE, pieces.Piece.WHITE, pieces.Piece.BLACK, pieces.Piece.BLACK]
+        for (x, y), color in zip(positions, colors):
+            chess_pieces[x][y] = pieces.Rook(x, y, color)
 
-        # Create Knights.
-        chess_pieces[1][Board.HEIGHT-1] = pieces.Knight(1, Board.HEIGHT-1, pieces.Piece.WHITE)
-        chess_pieces[Board.WIDTH-2][Board.HEIGHT-1] = pieces.Knight(Board.WIDTH-2, Board.HEIGHT-1, pieces.Piece.WHITE)
-        chess_pieces[1][0] = pieces.Knight(1, 0, pieces.Piece.BLACK)
-        chess_pieces[Board.WIDTH-2][0] = pieces.Knight(Board.WIDTH-2, 0, pieces.Piece.BLACK)
+    @staticmethod
+    def _create_knights(chess_pieces):
+        positions = [(1, Board.HEIGHT-1), (Board.WIDTH-2, Board.HEIGHT-1), (1, 0), (Board.WIDTH-2, 0)]
+        colors = [pieces.Piece.WHITE, pieces.Piece.WHITE, pieces.Piece.BLACK, pieces.Piece.BLACK]
+        for (x, y), color in zip(positions, colors):
+            chess_pieces[x][y] = pieces.Knight(x, y, color)
 
-        # Create Bishops.
-        chess_pieces[2][Board.HEIGHT-1] = pieces.Bishop(2, Board.HEIGHT-1, pieces.Piece.WHITE)
-        chess_pieces[Board.WIDTH-3][Board.HEIGHT-1] = pieces.Bishop(Board.WIDTH-3, Board.HEIGHT-1, pieces.Piece.WHITE)
-        chess_pieces[2][0] = pieces.Bishop(2, 0, pieces.Piece.BLACK)
-        chess_pieces[Board.WIDTH-3][0] = pieces.Bishop(Board.WIDTH-3, 0, pieces.Piece.BLACK)
+    @staticmethod
+    def _create_bishops(chess_pieces):
+        positions = [(2, Board.HEIGHT-1), (Board.WIDTH-3, Board.HEIGHT-1), (2, 0), (Board.WIDTH-3, 0)]
+        colors = [pieces.Piece.WHITE, pieces.Piece.WHITE, pieces.Piece.BLACK, pieces.Piece.BLACK]
+        for (x, y), color in zip(positions, colors):
+            chess_pieces[x][y] = pieces.Bishop(x, y, color)
 
-        # Create King & Queen.
-        chess_pieces[4][Board.HEIGHT-1] = pieces.King(4, Board.HEIGHT-1, pieces.Piece.WHITE)
-        chess_pieces[3][Board.HEIGHT-1] = pieces.Queen(3, Board.HEIGHT-1, pieces.Piece.WHITE)
-        chess_pieces[4][0] = pieces.King(4, 0, pieces.Piece.BLACK)
-        chess_pieces[3][0] = pieces.Queen(3, 0, pieces.Piece.BLACK)
-
-        return cls(chess_pieces, False, False)
+    @staticmethod
+    def _create_royalty(chess_pieces):
+        positions = [(4, Board.HEIGHT-1), (3, Board.HEIGHT-1), (4, 0), (3, 0)]
+        pieces_types = [pieces.King, pieces.Queen, pieces.King, pieces.Queen]
+        colors = [pieces.Piece.WHITE, pieces.Piece.WHITE, pieces.Piece.BLACK, pieces.Piece.BLACK]
+        for (x, y), piece_type, color in zip(positions, pieces_types, colors):
+            chess_pieces[x][y] = piece_type(x, y, color)
 
     def get_possible_moves(self, color):
         moves = []
         for x in range(Board.WIDTH):
             for y in range(Board.HEIGHT):
                 piece = self.chesspieces[x][y]
-                if (piece != 0):
-                    if (piece.color == color):
-                        moves += piece.get_possible_moves(self)
-
+                if piece != 0 and piece.color == color:
+                    moves += piece.get_possible_moves(self)
         return moves
 
     def perform_move(self, move):
         piece = self.chesspieces[move.xfrom][move.yfrom]
         self.move_piece(piece, move.xto, move.yto)
+        self._handle_special_moves(piece, move)
 
-        # If a pawn reaches the end, upgrade it to a queen.
-        if (piece.piece_type == pieces.Pawn.PIECE_TYPE):
-            if (piece.y == 0 or piece.y == Board.HEIGHT-1):
-                self.chesspieces[piece.x][piece.y] = pieces.Queen(piece.x, piece.y, piece.color)
+    def _handle_special_moves(self, piece, move):
+        if piece.piece_type == pieces.Pawn.PIECE_TYPE:
+            self._handle_pawn_promotion(piece)
+        elif piece.piece_type == pieces.King.PIECE_TYPE:
+            self._handle_king_move(piece, move)
 
-        if (piece.piece_type == pieces.King.PIECE_TYPE):
-            # Mark the king as having moved.
-            if (piece.color == pieces.Piece.WHITE):
-                self.white_king_moved = True
-            else:
-                self.black_king_moved = True
-            
-            # Check if king-side castling
-            if (move.xto - move.xfrom == 2):
-                rook = self.chesspieces[piece.x+1][piece.y]
-                self.move_piece(rook, piece.x+1, piece.y)
-            # Check if queen-side castling
-            if (move.xto - move.xfrom == -2):
-                rook = self.chesspieces[piece.x-2][piece.y]
-                self.move_piece(rook, piece.x+1, piece.y)
-    
+    def _handle_pawn_promotion(self, piece):
+        if piece.y == 0 or piece.y == Board.HEIGHT-1:
+            self.chesspieces[piece.x][piece.y] = pieces.Queen(piece.x, piece.y, piece.color)
+
+    def _handle_king_move(self, piece, move):
+        if piece.color == pieces.Piece.WHITE:
+            self.white_king_moved = True
+        else:
+            self.black_king_moved = True
+        self._handle_castling(piece, move)
+
+    def _handle_castling(self, piece, move):
+        if move.xto - move.xfrom == 2:  # King-side castling
+            rook = self.chesspieces[piece.x+1][piece.y]
+            self.move_piece(rook, piece.x+1, piece.y)
+        elif move.xto - move.xfrom == -2:  # Queen-side castling
+            rook = self.chesspieces[piece.x-2][piece.y]
+            self.move_piece(rook, piece.x+1, piece.y)
+
     def move_piece(self, piece, xto, yto):
         self.chesspieces[piece.x][piece.y] = 0
         piece.x = xto
         piece.y = yto
-
         self.chesspieces[xto][yto] = piece
 
-
-    # Returns if the given color is checked.
     def is_check(self, color):
-        other_color = pieces.Piece.WHITE
-        if (color == pieces.Piece.WHITE):
-            other_color = pieces.Piece.BLACK
-
+        other_color = pieces.Piece.BLACK if color == pieces.Piece.WHITE else pieces.Piece.WHITE
         for move in self.get_possible_moves(other_color):
             copy = Board.clone(self)
             copy.perform_move(move)
-
-            king_found = False
-            for x in range(Board.WIDTH):
-                for y in range(Board.HEIGHT):
-                    piece = copy.chesspieces[x][y]
-                    if (piece != 0):
-                        if (piece.color == color and piece.piece_type == pieces.King.PIECE_TYPE):
-                            king_found = True
-
-            if (not king_found):
+            if not any(piece != 0 and piece.color == color and piece.piece_type == pieces.King.PIECE_TYPE
+                       for row in copy.chesspieces for piece in row):
                 return True
-
         return False
 
-    # Returns piece at given position or 0 if: No piece or out of bounds.
     def get_piece(self, x, y):
-        if (not self.in_bounds(x, y)):
-            return 0
-
-        return self.chesspieces[x][y]
+        return self.chesspieces[x][y] if self.in_bounds(x, y) else 0
 
     def in_bounds(self, x, y):
-        return (x >= 0 and y >= 0 and x < Board.WIDTH and y < Board.HEIGHT)
+        return 0 <= x < Board.WIDTH and 0 <= y < Board.HEIGHT
 
     def to_string(self):
-        string =  "    A  B  C  D  E  F  G  H\n"
-        string += "    -----------------------\n"
+        board_str = "    A  B  C  D  E  F  G  H\n"
+        board_str += "    -----------------------\n"
         for y in range(Board.HEIGHT):
-            string += str(8 - y) + " | "
+            board_str += f"{8 - y} | "
             for x in range(Board.WIDTH):
                 piece = self.chesspieces[x][y]
-                if (piece != 0):
-                    string += piece.to_string()
-                else:
-                    string += ".. "
-            string += "\n"
-        return string + "\n"
+                board_str += piece.to_string() if piece != 0 else ".. "
+            board_str += "\n"
+        return board_str + "\n"
